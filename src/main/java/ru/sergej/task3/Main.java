cf4c9c906c0b6e0f99f149f7b356c3627f123c32
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-03-20 19:08:44 +0300

    changed class Person for testing code

--- src/main/java/ru/sergej/task3/Main.java
+++ src/main/java/ru/sergej/task3/Main.java
@@ -1,7 +1,34 @@
 package ru.sergej.task3;
 
+import ru.sergej.task3.entity.Person;
+import ru.sergej.task3.repositories.DataBaseHandler;
+import ru.sergej.task3.util.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+
+        try (Connection connection = ConnectionManager.open()) {
+            DataBaseHandler dataBaseHandler = new DataBaseHandler();
+
+
+            Person person1 = new Person("Иван", "Иванов", 30);
+            dataBaseHandler.save(connection, person1);
+            System.out.println("Person сохранен в базе данных: " + person1);
+
+
+            Person personToUpdate = new Person("Петр", "Петров", 25);
+            dataBaseHandler.save(connection, personToUpdate);
+            System.out.println("Person сохранен в базе данных: " + personToUpdate);
+
+            personToUpdate.setAge(26);
+            dataBaseHandler.update(personToUpdate, connection);
+            System.out.println("Person обновлен в базе данных: " + personToUpdate);
+        } catch (SQLException | IllegalArgumentException | IllegalAccessException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
---
+++ src/main/java/ru/sergej/task3/entity/Person.java
@@ -0,0 +1,68 @@
+package ru.sergej.task3.entity;
+
+import ru.sergej.task3.annotations.Column;
+import ru.sergej.task3.annotations.Id;
+import ru.sergej.task3.annotations.Table;
+
+@Table(name = "persons")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    int id;
+
+    @Column(name = "first_name")
+    String firstName;
+
+    @Column(name = "last_name")
+    String lastName;
+
+    @Column(name = "age")
+    int age;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Персона: id = %d\nИмя: %s, Фамилия: %s, Возраст: %d\n", id, firstName, lastName, age);
+    }
+}
