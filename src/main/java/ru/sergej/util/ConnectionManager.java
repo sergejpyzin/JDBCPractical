79e2d104c6abcba1072355892639a832ec62b1a8
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-03-20 10:10:57 +0300

    create ConnectionManager

---
+++ src/main/java/ru/sergej/util/ConnectionManager.java
@@ -0,0 +1,32 @@
+package ru.sergej.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    private static final String URL_KEY = "db.url";
+    private static final String LOGIN_KEY = "db.login";
+    private static final String PASSWORD_KEY = "db.password";
+    public static Connection open () {
+
+        try {
+
+
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(LOGIN_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY));
+
+        } catch (SQLException e) {
+
+            throw new RuntimeException(e);
+
+        }
+    }
+
+    private ConnectionManager() {}
+
+
+}
