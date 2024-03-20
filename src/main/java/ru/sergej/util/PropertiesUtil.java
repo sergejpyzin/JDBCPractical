e61ea8a50fccc95eac947740ed03494dd5d941d4
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-03-20 10:10:46 +0300

    create database.properties and PropertiesUtil

---
+++ src/main/java/ru/sergej/util/PropertiesUtil.java
@@ -0,0 +1,33 @@
+package ru.sergej.util;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesUtil {
+
+    private static final Properties PROPERTIES = new Properties();
+
+    static {
+        loadProperties();
+    }
+
+    private static void loadProperties() {
+
+        try (var InputStream = PropertiesUtil.class
+                .getClassLoader()
+                .getResourceAsStream("database.properties")) {
+            PROPERTIES.load(InputStream);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static String get(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+
+    private PropertiesUtil() {}
+
+}
